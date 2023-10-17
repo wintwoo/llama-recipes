9f7c8e8d5bb5d827a7bfb823ca668748542b0122
Injae Kwak <saltysoup@github.com>
2023-10-17 04:45:36 +0000

    updated torchrun param

--- examples/single_node.sh
+++ examples/single_node.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=llama2-finetuning-%u-%j
-#SBATCH --nodes=2
-#SBATCH --gres=gpu:8   
-#SBATCH --partition=l4
-#SBATCH --time=48:00:00
-
 # We grab the first node address to act as the head node for multi node training
-head_node=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+#head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+head_node_ip=127.0.0.1
 head_node_port=29500
 export MASTER_ADDR=$head_node_ip
 export MASTER_PORT=$head_node_port
@@ -27,17 +21,18 @@ export LOGLEVEL=INFO
 #export TORCH_DISTRIBUTED_DEBUG=DETAIL
 
 echo "setting HF token to download base model"
-HUGGINGFACE_TOKEN="<YOUR TOKEN>"
+#HUGGINGFACE_TOKEN="<YOUR TOKEN>"
+HUGGINGFACE_TOKEN="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"
 huggingface-cli login --token $HUGGINGFACE_TOKEN
 
-echo "starting training now.. on ${SLURM_NNODES} nodes with ${SLURM_GPUS} GPUs each"
+#echo "starting training now.. on ${SLURM_NNODES} nodes with ${SLURM_GPUS} GPUs each"
 
-cd $SLURM_SUBMIT_DIR
+#cd $SLURM_SUBMIT_DIR
 
-time srun torchrun --nnodes $SLURM_NNODES \
+time torchrun --nnodes 1 \
 	--nproc_per_node 8 \
 	--rdzv-endpoint "$MASTER_ADDR:$MASTER_PORT" \
-	--rdzv-id $SLURM_JOB_ID \
+	--rdzv-id foob \
 	--rdzv-backend c10d \
 	--log_dir logs \
-	finetuning.py --enable_fsdp --low_cpu_fsdp --model_name "$MODEL_NAME" --use_peft true --peft_method lora --quantization true --batch_size_training 1 --num_epochs 1 --dist_checkpoint_root_folder /bucket/model_checkpoints --dist_checkpoint_folder "$MODEL_NAME_NEW"
+	finetuning.py --model_name "$MODEL_NAME" --use_peft true --peft_method lora --quantization false --batch_size_training 1 --num_epochs 1 --dist_checkpoint_root_folder /bucket/model_checkpoints --dist_checkpoint_folder "$MODEL_NAME_NEW"
